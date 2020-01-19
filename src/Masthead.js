905ae35b5686ab4219045273eae1273b21742e7b
marcbrandon <43766461+marcbrandon@users.noreply.github.com>
2020-01-18 20:53:23 -0800

    Create months and map them to cards in a basic layout

---
+++ src/Masthead.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Masthead() {
+  return (
+    <div className="Header">
+          DMT-Calendar
+    </div>
+  );
+}
+
+export default Masthead;
---
+++ src/Month.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Months from "./months.json";
+
+const Month = () => (
+  <>
+    {Months.map(month => (
+      <MonthCard monthName={month.name} key={month.id} />
+    ))}
+  </>
+);
+
+class MonthCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    console.log("Month :" + this.props.monthName);
+    return (
+      <div className="Month" key={this.props.key}>
+        <p>{this.props.monthName}</p>
+        <div>days</div>
+      </div>
+    );
+  }
+}
+
+export default Month;
